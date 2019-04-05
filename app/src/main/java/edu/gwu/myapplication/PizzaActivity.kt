a9b554dd2993ef7bacf64eac37c3166e3f89ae3c
Katelyn Russo <katelynrusso@gwu.edu>
2019-04-05 17:56:19 -0400

    more screens

---
+++ app/src/main/java/edu/gwu/myapplication/PizzaActivity.kt
@@ -0,0 +1,27 @@
+package edu.gwu.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+
+class PizzaActivity : AppCompatActivity() {
+
+    private lateinit var orderButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pizza)
+        orderButton = findViewById(R.id.orderbutton)
+
+        orderButton.setOnClickListener {
+            Log.d("PizzaActivity", "Order Clicked")
+            /*
+            Place holder until explicit intent is implemented
+             */
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
